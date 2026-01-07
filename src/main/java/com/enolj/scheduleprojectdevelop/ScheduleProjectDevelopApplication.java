fb6dae69fa65eb0893ef05de9413d57adbd3583d
eNoLJ <derosatam76@gmail.com>
2026-01-07 10:06:22 +0900

    init commit

---
+++ src/main/java/com/enolj/scheduleprojectdevelop/ScheduleProjectDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.enolj.scheduleprojectdevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleProjectDevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleProjectDevelopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/enolj/scheduleprojectdevelop/ScheduleProjectDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enolj.scheduleprojectdevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleProjectDevelopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
